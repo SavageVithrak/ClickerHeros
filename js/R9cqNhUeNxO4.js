23cb4ea2f23e8f693824d15e9041581bf01be55b
SavageVithrak <99266733+SavageVithrak@users.noreply.github.com>
2022-10-06 10:30:46 -0400

    Add files via upload

---
+++ js/R9cqNhUeNxO4.js
@@ -0,0 +1,53 @@
+window.onload = function () {
+    $(".iconMenuMobile").on('click', function () {
+        $("#dropdownMenuMobile").slideToggle();
+    });
+}
+$("#expand").on('click', function () {
+    $("#iframehtml5").addClass("force_full_screen");
+    requestFullScreen(document.body);
+});
+
+$("#_exit_full_screen").on('click', cancelFullScreen);
+
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+
+function cancelFullScreen() {
+    $("#iframehtml5").removeClass("force_full_screen");
+    var requestMethod = document.cancelFullScreen || document.webkitCancelFullScreen || document.mozCancelFullScreen || document.exitFullScreenBtn;
+    if (requestMethod) { // cancel full screen.
+        requestMethod.call(document);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+
+if (document.addEventListener) {
+    document.addEventListener('webkitfullscreenchange', exitHandler, false);
+    document.addEventListener('mozfullscreenchange', exitHandler, false);
+    document.addEventListener('fullscreenchange', exitHandler, false);
+    document.addEventListener('MSFullscreenChange', exitHandler, false);
+}
+
+function exitHandler() {
+    if (document.webkitIsFullScreen === false
+            || document.mozFullScreen === false
+            || document.msFullscreenElement === false) {
+        cancelFullScreen();
+    }
+}
